748c4d4deb84e40b14a40c0ffa3a3d10a847c805
Erik L. Vonderscheer <evonder@gmail.com>
2008-11-22 22:34:32 +0000

    r19 Updated Expression List

--- TradeFilter.lua
+++ TradeFilter.lua
@@ -16,6 +16,8 @@ Versioning:
 				- LFG
 		-r16
 				- SAY Channel
+		-r20
+				- Release
 	v1.0
 			- Initial release for WotLK
 			- Added Custom Filter option
@@ -51,7 +53,7 @@ local L = Rock("LibRockLocale-1.0"):GetTranslationNamespace("TradeFilter")
 TradeFilter = Rock:NewAddon("TradeFilter", "LibRockDB-1.0", "LibRockConfig-1.0", "LibRockEvent-1.0", "LibRockHook-1.0", "LibRockTimer-1.0", "LibRockConsole-1.0")
 
 local MAJOR_VERSION = "1.1"
-local MINOR_VERSION = 000 + tonumber(("$Revision: 17 $"):match("%d+"))
+local MINOR_VERSION = 000 + tonumber(("$Revision: 20 $"):match("%d+"))
 TradeFilter.version = MAJOR_VERSION-- .. " r" .. MINOR_VERSION
 TradeFilter.date = string.sub("$Date: 2008-11-21 12:00:00 -0800 (Fri, 21 Nov 2008) $", 8, 17)
 
