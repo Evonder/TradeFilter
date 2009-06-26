b545918eb3a47120cc77fb585fd961e371e8ed34
Erik L. Vonderscheer <evonder@gmail.com>
2009-06-26 05:39:57 +0000

    Locale update

--- TradeFilter3Locale.lua
+++ TradeFilter3Locale.lua
@@ -19,3 +19,15 @@ if L then
 --@localization(locale="deDE", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
 if GetLocale() == "deDE" then return end
 end
+
+local L =  LibStub("AceLocale-3.0"):NewLocale("TradeFilter3", "zhCN", true, debug)
+if L then
+--@localization(locale="zhCN", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+if GetLocale() == "zhCN" then return end
+end
+
+local L =  LibStub("AceLocale-3.0"):NewLocale("TradeFilter3", "zhTW", true, debug)
+if L then
+--@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+if GetLocale() == "zhTW" then return end
+end
