86a81866e3ecd510fb6f6aec438d2f318ffcce96
Erik Vonderscheer <evonder@gmail.com>
2016-12-23 15:10:49 -0800

    More testing on WowAce locale substitutions

--- TradeFilter3Locale.lua
+++ TradeFilter3Locale.lua
@@ -11,7 +11,11 @@ debug = true
 local L =  LibStub("AceLocale-3.0"):NewLocale("TradeFilter3", "enUS", true)
 if L then
 --@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="subtable")@
+--
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat", namespace-delimiter=".")@
+--
 --@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="FILTERS", handle-subnamespaces="subtable")@
+--
 --@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, namespace="FILTERS/BASE", handle-subnamespaces="subtable")@
 if GetLocale() == "enUS" then return end
 end
