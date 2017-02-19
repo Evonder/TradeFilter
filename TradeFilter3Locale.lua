460335a529b09da73a7659a6bd1996600cbeb343
Erik Vonderscheer <evonder@gmail.com>
2017-02-19 12:02:22 -0800

    Trying handle-namspaces="concat"

--- TradeFilter3Locale.lua
+++ TradeFilter3Locale.lua
@@ -10,7 +10,7 @@ debug = true
 
 local L =  LibStub("AceLocale-3.0"):NewLocale("TradeFilter3", "enUS", true)
 if L then
---@localization(locale="enUS", format="lua_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="subtable")@
+--@localization(locale="enUS", format="lua_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="concat", namespace-delimiter=".")@
 if GetLocale() == "enUS" then return end
 end
 
