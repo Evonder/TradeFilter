4d81b62532b4d09bc8f8de39b42f64bb7f22a237
Erik Vonderscheer <evonder@gmail.com>
2020-05-10 02:21:01 -0700

    Localization Change handle-subnamespaces to "concat"

--- TradeFilter3Locale.lua
+++ TradeFilter3Locale.lua
@@ -10,42 +10,42 @@ debug = true
 
 local L =  LibStub("AceLocale-3.0"):NewLocale("TradeFilter3", "enUS", true)
 if L then
---@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="subtable")@
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="concat")@
 if GetLocale() == "enUS" then return end
 end
 
 local L =  LibStub("AceLocale-3.0"):NewLocale("TradeFilter3", "deDE")
 if L then
---@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="subtable")@
+--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="concat")@
 if GetLocale() == "deDE" then return end
 end
 
 local L =  LibStub("AceLocale-3.0"):NewLocale("TradeFilter3", "zhCN")
 if L then
---@localization(locale="zhCN", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="subtable")@
+--@localization(locale="zhCN", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="concat")@
 if GetLocale() == "zhCN" then return end
 end
 
 local L =  LibStub("AceLocale-3.0"):NewLocale("TradeFilter3", "zhTW")
 if L then
---@localization(locale="zhTW", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="subtable")@
+--@localization(locale="zhTW", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="concat")@
 if GetLocale() == "zhTW" then return end
 end
 
 local L =  LibStub("AceLocale-3.0"):NewLocale("TradeFilter3", "koKR")
 if L then
---@localization(locale="koKR", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="subtable")@
+--@localization(locale="koKR", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="concat")@
 if GetLocale() == "koKR" then return end
 end
 
 local L =  LibStub("AceLocale-3.0"):NewLocale("TradeFilter3", "frFR")
 if L then
---@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="subtable")@
+--@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="concat")@
 if GetLocale() == "frFR" then return end
 end
 
 local L =  LibStub("AceLocale-3.0"):NewLocale("TradeFilter3", "ruRU")
 if L then
---@localization(locale="ruRU", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="subtable")@
+--@localization(locale="ruRU", format="lua_additive_table", handle-unlocalized="english", same-key-is-true=true, handle-subnamespaces="concat")@
 if GetLocale() == "ruRU" then return end
 end
